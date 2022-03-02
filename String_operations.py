63324a3807b2a03493b5923ca623e79818f7542f
Sushantsingh2804 <sushantsinghruhil@gmail.com>
2022-03-02 15:05:06 +0530

    String operations

---
+++ String_operations.py
@@ -0,0 +1,7 @@
+first_name = input("Enter your first name:-")
+
+print("Your name starts with: ", first_name[0])
+print("Next three letters are : ", first_name[1:4])
+
+last_name = input("Enter your last name:-")
+print("Your full name is : ", (first_name+" "+last_name))
