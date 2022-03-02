2bc6389e72a590f72aa088ec44c9611039bb51c2
Sushantsingh2804 <sushantsinghruhil@gmail.com>
2022-03-02 18:37:29 +0530

    Assignment 02-03-2022

---
+++ Assignment_02-03-2022/Program_4.py
@@ -0,0 +1,3 @@
+# WAP to accept a string and count the frequency of each vowel.
+User_input = input("Enter a phrase: ")
+print(User_input.count('a')+User_input.count('e')+User_input.count('i')+User_input.count('o')+User_input.count('u'))
--- String_operations.py
+++ String_operations.py
@@ -4,4 +4,10 @@ print("Your name starts with: ", first_name[0])
 print("Next three letters are : ", first_name[1:4])
 
 last_name = input("Enter your last name:-")
-print("Your full name is : ", (first_name+" "+last_name))
+full_name = first_name+" "+last_name
+print("Your full name is : ", full_name)
+
+print("Upper case: ", full_name.upper())
+print("Lower case: ", full_name.lower())
+
+
