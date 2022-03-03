c456eb66902b3ca20059b38561a60978c66fef7c
Sushantsingh2804 <sushantsinghruhil@gmail.com>
2022-03-03 23:13:09 +0530

    Assignment 03-03-2022

---
+++ Assignment_03-03-2022/Program_2.py
@@ -0,0 +1,7 @@
+# WAP to print if a String is a palindrome or not
+User_input = input("Enter a phrase : ")
+Reverse_string = User_input[::-1]
+if User_input == Reverse_string:
+    print("It is a PALINDROME")
+else:
+    print("It is NOT a palindrome")
---
+++ Assignment_03-03-2022/Program_4.py
@@ -0,0 +1,3 @@
+# WAP to read the employee data name age designation salary from user and display
+print(("Employee Name:- "+input("Enter the Name: ")+"\n") + ("Employee Age:- "+input("Enter Age: ")+"\n") +
+      ("Employee Designation:- "+input("Enter Designation: ")+"\n") + ("Employee Salary:- "+input("Enter the Salary: ")))
